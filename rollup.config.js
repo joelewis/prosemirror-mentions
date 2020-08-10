11f30c6a8bd99195c38cdefe7f7f6e84b9f22ed2
Josh Lind <joshua.lind@maven.io>
2020-08-10 13:59:12 -0700

    build-able

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,20 @@
-import babel from 'rollup-plugin-babel';
+import babel from "rollup-plugin-babel";
 
 export default {
-  input: './src/index.js',
-  output: [{
-    format: 'cjs',
-    file: 'dist/index.js',
-  }, {
-    file: 'dist/index.es.js',
-    format: 'esm'
-  }],
+  input: "./src/index.js",
+  output: [
+    {
+      format: "cjs",
+      file: "dist/index.js"
+    },
+    {
+      file: "dist/index.es.js",
+      format: "es"
+    }
+  ],
   sourcemap: true,
   plugins: [babel()],
   external(id) {
     return !/^[\.\/]/.test(id);
-  },
+  }
 };
